fb9511ab9a920a3545a004bbf6e542cb37a1bd13
omar-caiguan-ojeda <leo.caiguan.ojeda@gmail.com>
2025-01-19 09:39:40 -0300

    Configurando Categories, parte 3, adjuntando dir .env y vinculando archivo json

--- frontend/app/(store)/[categoryId]/page.tsx
+++ frontend/app/(store)/[categoryId]/page.tsx
@@ -1,11 +1,18 @@
-
 type Params = Promise<{categoryId: string}>
+
+
+async function getProducts(categoryId: string) {
+    const url = `${process.env.API_URL}/categories/${categoryId}?products=true`
+    const req = await fetch(url)
+    const json = await req.json()
+    console.log(json)
+}
 
 export default async function StorePage({params}: {params: Params}) {
 
     const { categoryId } = await params
 
-    console.log(categoryId)
+    await getProducts(categoryId)
 
     return (
         <div>StorePage</div>
