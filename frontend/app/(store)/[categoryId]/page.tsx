b38a1c555a371e3526e2001fae2e23e3d8c9555f
omar-caiguan-ojeda <leo.caiguan.ojeda@gmail.com>
2025-01-19 07:02:35 -0300

    Configurando redireccionamiento de categories, parte 1

--- frontend/app/(store)/[categoryId]/page.tsx
+++ frontend/app/(store)/[categoryId]/page.tsx
@@ -1,6 +1,12 @@
-import React from "react";
 
-export default function StorePage() {
+type Params = Promise<{categoryId: string}>
+
+export default async function StorePage({params}: {params: Params}) {
+
+    const { categoryId } = await params
+
+    console.log(categoryId)
+
     return (
         <div>StorePage</div>
     )
